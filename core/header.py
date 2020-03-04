9f687a4a48cfb95bcadfe8702eb95fface69d521
darkz3ro <darkz3ro@localhost.localdomain>
2020-03-04 18:43:39 +0200

    Name and allignment

--- core/header.py
+++ core/header.py
@@ -6,16 +6,16 @@ def Banner2():
 
 
 def Banner():
-    print """                                          
-            88             88             
-            ""             ""                           
+    print """
+            88             88
+            ""             ""
                                               8888      8888
 8b,dPPYba,  88 8b,dPPYba,  88 ,adPPYYba,    88        88    88
 88P'   `"8a 88 88P'   `"8a 88 ""     `Y8    88              88
 88       88 88 88       88 88 ,adPPPPP88    88              88
 88       88 88 88       88 88 88,    ,88    88            88
-88       88 88 88       88 88 `"8bbdP"Y8    88          88  
+88       88 88 88       88 88 `"8bbdP"Y8    88          88
                           ,88                 8888      888888
                         888P" 		      """
     print "                                  Version %s" % config.VERSION
-    print "\nNinja C2 | Stealthy Pwn like a Ninja"
+    print "\nNinja C2 | Stealthy Pwn like a Ninja\n\n"
