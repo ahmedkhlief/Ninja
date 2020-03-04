a547ff1639710608f577451d25012badd7bc4840
darkz3ro <darkz3ro@localhost.localdomain>
2020-03-04 18:45:08 +0200

    remove old banner

--- core/header.py
+++ core/header.py
@@ -1,9 +1,6 @@
 # Embedded file name: core\header.py
 from core import config
 
-def Banner2():
-    print '\n\n   +--------------------------------------------------------MUDDY C3-\n   :". /  /  /\n   :.-". /  /\n   : _.-". /\n   :"  _.-".\n   :-""     ".\n   :\n   :\n ^.-.^\n\'^\\+/^`\n\'/`"\'\\`\t                                                Version : {%s}\n\n\t\t                                      \n\n ' % config.VERSION
-
 
 def Banner():
     print """
