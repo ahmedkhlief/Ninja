80e33346b74ef5213067b62fc91db33cd34dee18
ahmed khlief <ahmed.khlief@gmail.com>
2021-04-15 00:08:57 +0300

    Updated install script for python3

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-apt-get install python
-apt-get install python-pip
+apt-get install python3
+apt-get install python3-pip
 apt-get install mono-devel
-pip install pycrypto
-pip install cheroot
-pip install flask
+pip3 install pycrypto
+pip3 install cheroot
+pip3 install flask
+pip3 install donut-shellcode==0.9.2
